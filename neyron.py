2a917627691e38370ac3a0ae4d4fa07c9fcbb18c
jellyb0y <zuizberg.reg@gmail.com>
2019-11-16 15:56:41 +0300

    Последняя версия

--- neyron.py
+++ neyron.py
@@ -10,76 +10,88 @@ class Exceptions():
 		InvalidDim = Exception('Invalid dimention of vector')
 
 
-class Neyron():
+class Neuron():
 	'''
 		Класс нейрона
 	'''
 
 
-	def __init__(self, inputs, a):
-		if not isinstance(inputs, int) or not isinstance(a, float):
+	def __init__(self, inputs_count, a_coef):
+		if not isinstance(inputs_count, int) or not isinstance(a_coef, float):
 			raise ValueError
 
-		if inputs < 1:
+		if inputs_count < 1:
 			raise Exceptions.InvalidInputsCount
 
-		self.inputs = inputs
-		self.a = a
-		self.w = self.__gen_w(inputs)
+		self.inputs_count = inputs_count
+		self.a_coef = a_coef
+		self.weights = self.gen_weight(self.inputs_count + 1)
 
 
-	def __gen_w(self, inputs):
-		arr = []
-		for _ in range(inputs):
-			arr.append(random())
+	def gen_weight(self, inputs_count):
+		array = []
+		for _ in range(inputs_count):
+			array.append(random())
 
-		return arr
+		return array
 
 
-	def __func(self, sum):
+	def func(self, sum_):
 		'''
 			Метод вычисления эксп. функции
 		'''
-		a = self.a
-		return 1 / (1 + exp(-a * sum))
+		return 1 / (1 + exp(-self.a_coef * sum_))	
 
 
-	def __func_disc(self, sum_):
-		sigmoid = self.__func(sum_)
+	def sum(self, x, weights):
+		'''
+			Сумматор
+		'''
+		sum_ = 1 * weights[0]
+		for i in range(len(x)):
+			sum_ += x[i] * weights[i + 1]
+
+		return sum_
+
+
+	def calc(self, x, weights=None):
+		'''
+			Внешний открытый метод вычисления выходного сигнала
+		'''
+		if not weights is None:
+			if not isinstance(weights, list):
+				raise ValueError
+			if len(self.weights) != self.inputs_count + 1:
+				raise Exceptions.InvalidDim
+			self.weights = weights
+
+		if not isinstance(x, list):
+			raise ValueError
+
+		if len(x) != self.inputs_count:
+			raise Exceptions.InvalidDim
+
+		sum_ = self.sum(x, self.weights)
+		result = self.func(sum_)
+
+		return result
+
+
+class NeuronDigit(Neuron):
+	'''
+		Класс нейрона
+		Возвращает 1, 0 или -1
+	'''
+
+
+	def func(self, sum_):
+		'''
+			Метод вычисления эксп. функции
+		'''
+		sigmoid = Neuron.func(self, sum_)
+
 		if sigmoid > 0.75:
 			return 1
 		elif sigmoid <= 0.75 and sigmoid >= 0.25:
 			return 0
 		else: return -1
-
-
-	def __sum(self, x, w):
-		'''
-			Сумматор
-		'''
-		k = 0
-		sum_ = 0
-		for i in x:
-			sum_ += i * w[k]
-			k += 1
-
-		return sum_
-
-
-	def calc(self, x, w=None):
-		'''
-			Внешний открытый метод вычисления выходного сигнала
-		'''
-		if w is None:
-			w = self.__gen_w(self.inputs)
-
-		if not isinstance(x, list) or not isinstance(w, list):
-			raise ValueError
-
-		if len(x) != self.inputs or len(w) != self.inputs:
-			raise Exceptions.InvalidDim
-
-		sum_ = self.__sum(x, w)
-		result = self.__func_disc(sum_)
-
-		return result
--- test.neuron.py
+++ test.neuron.py
@@ -1,38 +1,79 @@
-from neyron import Neyron
+from neyron import NeuronDigit
 from time import sleep
 
-neyron = Neyron(3, 1.0)
+neyron = NeuronDigit(4, 1.0)
 
 a = [
-	[1, 1, 0],
-	[1, 0, 1],
-	[1, 1, 1]
+	[1, 1, 1, 1],
+	[1, 1, 0, 1],
+	[0, 1, 1, 0],
+	[0, 1, 1, 1],
+	[0, 1, 0, 0],
+	[0, 1, 0, 1],
+	[1, 1, 1, 0],
+	[1, 1, 1, 1],
+	[0, 0, 1, 0],
+	[0, 0, 1, 1],
+	[1, 0, 0, 0],
+	[1, 0, 0, 1],
+	[1, 0, 1, 0],
+	[1, 0, 1, 1],
+	[0, 0, 0, 0],
+	[0, 0, 0, 1],
+	[1, -1, 0, 0],
+	[1, -1, 0, 1],
+	[0, -1, 1, 0],
+	[0, -1, 1, 1],
+	[0, -1, 0, 0],
+	[0, -1, 0, 1],
+	[1, -1, 1, 0],
+	[1, -1, 1, 1],
 ]
 
-b = [0, 0, 1]
+b = [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, -1, -1, -1, -1, -1, -1, -1, -1]
+
+def learn(a, b, w):
+	learning_constant = 0.0001
+	error = b - neyron.calc(a, w)
+	for i in range(len(w)):
+		if i == 0:
+			w[0] += error * learning_constant
+		else: 
+			w[i] += error * learning_constant * a[i - 1]
+
+	return w
+
 
 def sum_miss(w=None):
-	i = 0
 	sum_ = 0
-	for k in a:
-		result = neyron.calc(k, w)
+	for i in range(len(a)):
+		result = neyron.calc(a[i], w)
 		miss = (b[i] - result)**2
-		i += 1
 		sum_ += miss
  
 	return sum_
 
-w = neyron.w
-acc = 0.000001
-last = 0
-cur = 100
-while cur**2 > 0.001:
-	cur = sum_miss(w)
-	print(cur, w)
-	for i in range(len(w)):
-		direct = b[i] - neyron.calc(a[i], w)
-		w[i] += acc * direct
 
-	last = cur
+def learning(w):
+	error = None
+	while error != 0:
+		for i in range(len(a)):
+			w = learn(a[i], b[i], w)
+		error = sum_miss(w)
+		print(error, w)
+	return w
+
+w = learning(neyron.weights)
 
 print('Result: ', w)
+
+for i in range(len(a)):
+	print(a[i], neyron.calc(a[i], w), ' - ', b[i])
+
+while True:
+	[x, y, z, f] = input('[x, y, z, f]: ').split(' ')
+	x = int(x)
+	y = int(y)
+	z = int(z)
+	f = int(f)
+	print(neyron.calc([x, y, z, f]))
